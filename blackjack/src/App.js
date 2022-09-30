43bcf347249822d3fa50b1fcdf37f4b3b7461489
John Doe <tirunaharipandu@gmail.com>
2022-09-30 20:10:43 +0530

    second commit
    Co-authored-by: Pratyush Kongalla <pratyush3124@users.noreply.github.com>

---
+++ blackjack/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./Nav";
+import Main from "./Main"
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
